c795c520fea810672660e71073db3dedeee72f2f
Eric Prykhodko <eprykhodko@getgoing.com>
2017-05-13 19:12:26 +0300

    tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -7,3 +7,18 @@ class SortedKeysCheckerTestCase(unittest.TestCase):
 
     def test_checker_implements_interface(self):
         getattr(SortedKeysChecker, 'run')
+
+    def test_unsorted_keys(self):
+        pass
+
+
+class SortedKeysCheckerNegativeTestCase(unittest.TestCase):
+
+    def test_single_line_dict(self):
+        pass
+
+    def test_numeric_keys(self):
+        pass
+
+    def test_embedded_unpacking(self):
+        pass
