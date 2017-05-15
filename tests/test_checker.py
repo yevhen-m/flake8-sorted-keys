76855f781feaf1765231a38c8b500b6f71104d80
Eric Prykhodko <eprykhodko@getgoing.com>
2017-05-16 00:49:22 +0300

    tests

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -1,7 +1,11 @@
 import ast
+import sys
 import unittest
 
 from flake8_sorted_keys import SortedKeysChecker
+
+
+ver_maj, ver_min = sys.version_info.major, sys.version_info.minor
 
 
 class SortedKeysCheckerBaseTestCase(unittest.TestCase):
@@ -21,9 +25,35 @@ class SortedKeysCheckerPositiveBaseTestCase(SortedKeysCheckerBaseTestCase):
                     'b': 'b',
                   }'''
         lint_errors = self.check_snippet(code)
-        for error in lint_errors:
-            offset, line, msg, _ = error
-            self.assertIn('S001', msg)
+
+        self.assertEqual(len(lint_errors), 1)
+        first_error = lint_errors[0]
+        line, offset, msg, _ = first_error
+        self.assertIn('S001', msg)
+
+    def test_unsorted_keys_embedded_dicts(self):
+        code = '''dict_literal = {   # 1  <--
+                    'a': 'a',        # 2
+                    'c': 'c',        # 3
+                    'd': {           # 4  <--
+                        'd': 'd',    # 5
+                        'f': 'f',    # 6
+                        'b': 'b',    # 7
+                    },               # 8
+                    'b': 'b',        # 9
+                  }'''
+        lint_errors = self.check_snippet(code)
+
+        self.assertEqual(len(lint_errors), 2)
+        first_error, second_error = lint_errors
+
+        line, offset, msg, _ = first_error
+        self.assertIn('S001', msg)
+        self.assertEqual(line, 1)
+
+        line, offset, msg, _ = second_error
+        self.assertIn('S001', msg)
+        self.assertEqual(line, 4)
 
 
 class SortedKeysCheckerNegativeBaseTestCase(SortedKeysCheckerBaseTestCase):
@@ -36,6 +66,18 @@ class SortedKeysCheckerNegativeBaseTestCase(SortedKeysCheckerBaseTestCase):
 
     def test_numeric_keys(self):
         code = '''dict_literal = {'1': 'a', '3':'c', '2': 'b'}'''
+        lint_errors = self.check_snippet(code)
+
+        self.assertFalse(lint_errors)
+
+    @unittest.skip(ver_maj > 2 and ver_min > 4)
+    def test_embedded_unpacking(self):
+        code = '''dict_literal = {
+                    'a': 'a',
+                    'c': 'c',
+                    **some_dict,
+                    'b': 'b',
+                  }'''
         lint_errors = self.check_snippet(code)
 
         self.assertFalse(lint_errors)
